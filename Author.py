e46d8418acb7c974e674b5bcbc1d938731437f34
ruthsarah2000 <160377609+ruthsarah2000@users.noreply.github.com>
2024-04-21 23:29:59 -0500

    Update Author.py

--- Author.py
+++ Author.py
@@ -1,1 +1,11 @@
+# author.py
+class Author:
+    def __init__(self, name, biography):
+        self.__name = name
+        self.__biography = biography
 
+    def get_name(self):
+        return self.__name
+
+    def get_biography(self):
+        return self.__biography
