31327ff9dbb1aa827e80f647be5ea816901b5c64
ruthsarah2000 <160377609+ruthsarah2000@users.noreply.github.com>
2024-04-21 23:29:17 -0500

    Update LibraryManagementSystem.py

--- LibraryManagementSystem.py
+++ LibraryManagementSystem.py
@@ -1,1 +1,32 @@
+class Book:
+    def __init__(self, title, author, isbn, genre, publication_date):
+        self.__title = title
+        self.__author = author
+        self.__isbn = isbn
+        self.__genre = genre
+        self.__publication_date = publication_date
+        self.__availability = True
 
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def get_isbn(self):
+        return self.__isbn
+
+    def get_genre(self):
+        return self.__genre
+
+    def get_publication_date(self):
+        return self.__publication_date
+
+    def is_available(self):
+        return self.__availability
+
+    def borrow(self):
+        self.__availability = False
+
+    def return_book(self):
+        self.__availability = True
