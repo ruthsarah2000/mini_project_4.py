c187e0e7d6488279a7a39a090299f87d5d2d908a
ruthsarah2000 <160377609+ruthsarah2000@users.noreply.github.com>
2024-04-21 23:28:27 -0500

    Update User.py

--- User.py
+++ User.py
@@ -1,1 +1,22 @@
+# user.py
+class User:
+    def __init__(self, name, library_id):
+        self.__name = name
+        self.__library_id = library_id
+        self.__borrowed_books = []
 
+    def get_name(self):
+        return self.__name
+
+    def get_library_id(self):
+        return self.__library_id
+
+    def get_borrowed_books(self):
+        return self.__borrowed_books
+
+    def borrow_book(self, book):
+        self.__borrowed_books.append(book)
+
+    def return_book(self, book):
+        if book in self.__borrowed_books:
+            self.__borrowed_books.remove(book)
