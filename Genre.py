d18868f30cc73403f2337d66d63de9ddb4a44c35
ruthsarah2000 <160377609+ruthsarah2000@users.noreply.github.com>
2024-04-21 23:30:38 -0500

    Update Genre.py

--- Genre.py
+++ Genre.py
@@ -1,1 +1,15 @@
+# genre.py
+class Genre:
+    def __init__(self, name, description, category):
+        self.__name = name
+        self.__description = description
+        self.__category = category
 
+    def get_name(self):
+        return self.__name
+
+    def get_description(self):
+        return self.__description
+
+    def get_category(self):
+        return self.__category
